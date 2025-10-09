65f1746edc8788f9b45b722bc9ce39cab995d90c
Vera <althinivera77@gmail.com>
2025-10-09 16:35:44 +0200

    Created more files for structure. Made a todo to create a browserrouter in main.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
 
+// TODO: Lägga in en browserrouter här
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
