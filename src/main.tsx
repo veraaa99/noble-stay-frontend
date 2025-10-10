3351b2742d9d96e650baeb93d0879ed40655593c
Vera <althinivera77@gmail.com>
2025-10-10 17:00:23 +0200

    Created a BookingLayout and UserLayout, and added them to the BrowserRouter.

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router'
+import {createBrowserRouter, RouterProvider } from 'react-router'
 import './index.css'
-import App from './App.tsx'
+// import App from './App.tsx'
 import RootLayout from './layouts/RootLayout'
 import Home from './pages/Home.tsx'
 import SearchResults from './pages/SearchResults.tsx'
 import Details from './pages/Details.tsx'
 import NotFound from './pages/NotFound.tsx'
-
-// TODO: Fix UserLayout
+import BookingLayout from './layouts/BookingLayout.tsx'
+import PlaceBooking from './pages/PlaceBooking.tsx'
+import BookingConfirmed from './pages/BookingConfirmed.tsx'
+import UserLayout from './layouts/UserLayout.tsx'
+import Profile from './pages/Profile.tsx'
 
 const router = createBrowserRouter([
   {
@@ -17,6 +20,7 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     children: [
       {
+
         index: true,
         element: <Home />
       },
@@ -31,7 +35,32 @@ const router = createBrowserRouter([
       {
         path: '*',
         element: <NotFound />
+      },
+
+      {
+        element: <BookingLayout />,
+        children: [
+          {
+            path: 'book',
+            element: <PlaceBooking />
+          },
+          {
+            path: 'confirmed',
+            element: <BookingConfirmed />
+          }
+        ]
+      },
+
+      {
+        element: <UserLayout />,
+        children: [
+          {
+            path: 'profile',
+            element: <Profile />
+          }
+        ]
       }
+
     ]
   }
 ])
