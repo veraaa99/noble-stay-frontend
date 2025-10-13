af0c2c9ea2a257bb1147b9ad35b001b56d51ea61
Vera <althinivera77@gmail.com>
2025-10-13 16:33:35 +0200

    Added html structure for SearchResults page

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,6 +1,95 @@
 const SearchResults = () => {
   return (
-    <div>SearchResults</div>
+    <div>
+      {/* Search filters */}
+      <div>
+        <div>
+          <h1>Showing results for:</h1>
+          <p>DummyLocation</p>
+          <p>DummyDate</p>
+        </div>
+        <div>
+          <p>dummyNumber</p>
+          <button>Filter</button>
+        </div>
+      </div>
+
+      {/* Search results */}
+      <div>
+
+        {/* Castle card/s */}
+          <div>
+            <div>
+              <div>
+                <img src="" alt="" />
+              </div>
+              <div>
+                <div>
+                  <h2>DummyTitle</h2>
+                  <p>DummyLocation</p>
+                </div>
+                <p>DummyEventAvaliable</p>
+                <div>
+                  <p>DummyDescription</p>
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div>
+              <p>DummyPrice</p>
+              <button>Read more</button>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <div>
+                <img src="" alt="" />
+              </div>
+              <div>
+                <div>
+                  <h2>DummyTitle</h2>
+                  <p>DummyLocation</p>
+                </div>
+                <p>DummyEventAvaliable</p>
+                <div>
+                  <p>DummyDescription</p>
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div>
+              <p>DummyPrice</p>
+              <button>Read more</button>
+            </div>
+          </div>
+
+          <div>
+            <div>
+              <div>
+                <img src="" alt="" />
+              </div>
+              <div>
+                <div>
+                  <h2>DummyTitle</h2>
+                  <p>DummyLocation</p>
+                </div>
+                <p>DummyEventAvaliable</p>
+                <div>
+                  <p>DummyDescription</p>
+                </div>
+              </div>
+            </div>
+            <hr />
+            <div>
+              <p>DummyPrice</p>
+              <button>Read more</button>
+            </div>
+          </div>
+
+      </div>
+
+    </div>
   )
 }
 export default SearchResults
